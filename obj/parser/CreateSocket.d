e1b7dd0694a8d7a6322656b79539174171081ee0
LE Thomas <thomas.le.tle@gmail.com>
2024-06-21 18:58:46 +0200

    added new stuff

--- obj/parser/CreateSocket.d
+++
@@ -1,6 +0,0 @@
-obj/parser/CreateSocket.o: srcs/parser/CreateSocket.cpp \
-  include/headers_hpp/ServerConf.hpp include/headers_hpp/Location.hpp
-
-include/headers_hpp/ServerConf.hpp:
-
-include/headers_hpp/Location.hpp:
