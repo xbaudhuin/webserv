7da2067ce217ab598c667a7c49a9535a69ec0db0
xbaudhuin <x.baudhuin2@gmail.com>
2024-07-05 17:54:05 +0200

    il faut un message

--- obj/main/main.d
+++ obj/main/main.d
@@ -1,4 +1,4 @@
-obj/main/Webserv.o: srcs/main/Webserv.cpp include/headers_hpp/Webserv.hpp \
+obj/main/main.o: srcs/main/main.cpp include/headers_hpp/Webserv.hpp \
  include/headers_hpp/Config.hpp include/headers_hpp/ServerConf.hpp \
  include/headers_hpp/Location.hpp include/headers_hpp/Typedef.hpp \
  include/headers_hpp/Utils.hpp include/headers_hpp/Error.hpp \
