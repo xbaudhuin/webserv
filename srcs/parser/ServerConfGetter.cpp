42e69a707b6741cf2abc0f2576558239022fe939
LE Thomas <thomas.le.tle@gmail.com>
2024-07-09 18:48:44 +0200

    better precise location

--- srcs/parser/ServerConfGetter.cpp
+++ srcs/parser/ServerConfGetter.cpp
@@ -1,4 +1,5 @@
 #include "ServerConf.hpp"
+#include "Error.hpp"
 
 std::string &ServerConf::getIndexErrorPage(int errorCode)
 {
@@ -47,12 +48,57 @@ vec_location& ServerConf::getLocations(void){
 Location& ServerConf::getPreciseLocation(const std::string &url)
 {
     size_t size = this->_locations.size();
+    std::string s = this->root + url;
     for (size_t i = 0; i < size; i++)
     {
-        if(this->_locations[i].getUrl() == url)
+        if(this->_locations[i].isExactMatch() && this->_locations[i].getUrl() == s)
+        {
+#ifdef PRINT
+            std::cout << YELLOW << "✨ JACKPOT ✨" << RESET << std::endl;
+            std::cout << RED << "FOUND IN PRECISE LOCATION" << std::endl;
+            std::cout << s << RESET << std::endl;
+#endif
             return(this->_locations[i]);
+        }
     }
-    throw std::logic_error("how wtf?");
+    size_t pos = s.size();
+    pos = s.find_last_of("/", pos);
+    std::string s1 = s.substr(0, pos + 1);
+    std::string s2;
+    if(s1 == s)
+    {
+#ifdef PRINT
+        std::cout << BLUE << "TWINNING !" << RESET << std::endl;
+#endif   
+    }
+    else
+    {
+        s2 = s.substr(pos, s.size());
+#ifdef PRINT
+        std::cout << GREEN << "EWW A FILE!" << std::endl;
+        std::cout << "This is the ugly file: " << s2 << RESET << std::endl;
+#endif
+    }
+    while (pos != std::string::npos)
+    {   
+        pos = s.find_last_of("/", pos);
+        std::string s1 = s.substr(0, pos + 1);
+        for (size_t i = 0; i < size; i++)
+        {
+            if(this->_locations[i].getUrl() == s)
+            {
+#ifdef PRINT
+                std::cout << RED << "FOUND IN NORMAL LOCATION" << std::endl;
+                std::cout << s1 << RESET << std::endl;
+#endif
+                return(this->_locations[i]);
+            }
+        }
+        pos--;
+        if(pos < this->root.size())
+            break;
+    }
+    throw std::logic_error("you idiot sandwich");
     return(this->_locations[0]);
 }
 
