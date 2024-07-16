95d6dc38ddc8a20ca5ee29029afac98f42ef493d
Xavier Baudhuin <xabaudhu@paul-f5Ar4s4.clusters.42paris.fr>
2024-07-16 18:33:04 +0200

    added directorylisting

---
+++ obj/parser/ParserLocation.d
@@ -0,0 +1,16 @@
+obj/parser/ParserLocation.o: srcs/parser/ParserLocation.cpp \
+  include/headers_hpp/ServerConf.hpp include/headers_hpp/Location.hpp \
+  include/headers_hpp/Utils.hpp include/headers_hpp/Typedef.hpp \
+  include/headers_hpp/Colors.hpp include/headers_hpp/Error.hpp
+
+include/headers_hpp/ServerConf.hpp:
+
+include/headers_hpp/Location.hpp:
+
+include/headers_hpp/Utils.hpp:
+
+include/headers_hpp/Typedef.hpp:
+
+include/headers_hpp/Colors.hpp:
+
+include/headers_hpp/Error.hpp:
---
+++ obj/parser/Printer.d
@@ -0,0 +1,14 @@
+obj/parser/Printer.o: srcs/parser/Printer.cpp \
+  include/headers_hpp/ServerConf.hpp include/headers_hpp/Location.hpp \
+  include/headers_hpp/Utils.hpp include/headers_hpp/Typedef.hpp \
+  include/headers_hpp/Colors.hpp
+
+include/headers_hpp/ServerConf.hpp:
+
+include/headers_hpp/Location.hpp:
+
+include/headers_hpp/Utils.hpp:
+
+include/headers_hpp/Typedef.hpp:
+
+include/headers_hpp/Colors.hpp:
