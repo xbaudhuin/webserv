1e87f30ef5fe2ed3f4a49f3b20ecd96665229061
Xavier Baudhuin <xabaudhu@paul-f5Ar4s4.clusters.42paris.fr>
2024-07-16 18:33:36 +0200

    fclean

--- obj/parser/Split.d
+++
@@ -1,8 +0,0 @@
-obj/parser/Split.o: srcs/parser/Split.cpp include/headers_hpp/Utils.hpp \
-  include/headers_hpp/Typedef.hpp include/headers_hpp/Colors.hpp
-
-include/headers_hpp/Utils.hpp:
-
-include/headers_hpp/Typedef.hpp:
-
-include/headers_hpp/Colors.hpp:
--- obj/parser/UtilsParser.d
+++
@@ -1,9 +0,0 @@
-obj/parser/UtilsParser.o: srcs/parser/UtilsParser.cpp \
-  include/headers_hpp/Utils.hpp include/headers_hpp/Typedef.hpp \
-  include/headers_hpp/Colors.hpp
-
-include/headers_hpp/Utils.hpp:
-
-include/headers_hpp/Typedef.hpp:
-
-include/headers_hpp/Colors.hpp:
