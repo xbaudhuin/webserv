6d4954b0c51fe72621653efcfc37f21b04304456
LE Thomas <thomas.le.tle@gmail.com>
2024-07-21 17:54:21 +0200

    cgi php

--- html/cgi/python/LogOut.py
+++ html/cgi/python/LogOut.py
@@ -1,27 +1,33 @@
+import os
+import datetime
 
-import os, datetime
 
-print("Content-type:text/html")
+def logOut():
+    print("Content-type: text/html")
+    body = "<!DOCTYPE html>"
+    body += "<html><head><title>Bye Bye!</title>"
+    body += "<style>* "
+    body += "{font-family: system-ui, -apple-system, BlinkMacSystemFont,"
+    body += "'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', "
+    body += "'Helvetica Neue', sans-serif;} body { background-color: "
+    body += "CornflowerBlue; display: flex; justify-content: center; "
+    body += "align-items: center; height: 100vh; margin: 0; flex-direction: "
+    body += "column;} h1 { font-size: 3em; color: black; } </style></head>"
+    body += "<body><h1>See Ya!</h1>"
+    body += "<br><a href=\"/python/welcome.py\"><button>Go back to main page"
+    body += "</button></a></body></html>"
+    if "SERVER_NAME" in os.environ:
+        serverName = os.environ.get('SERVER_NAME')
+        print(f"Server: {serverName}")
+    datetimenow = datetime.datetime.now()
+    date = datetimenow.astimezone().strftime("%a, %d %b %Y %H:%M:%S %Z")
+    print(f"Date: {date}")
+    time = "Thu, 01-Jan-1970 00:00:10 GMT"
+    print(f"Set-Cookie: user=blbkvvv;Expires={time}; Path=/")
+    print(f"Set-Cookie: bgcolor=spspspsp;Expires={time}; Path=/")
+    print(f"Content-Length: {len(body)}\r\n")
+    print(f"{body}")
 
-body = "<head><title> Bye Bye !"
-body += "</title><style>* {font-family: system-ui, -apple-system, BlinkMacSystemFont,"
-body += "'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;}"
-body += "body { background-color: "
-body += "CornflowerBlue"
-body += "; display: flex; justify-content: center; align-items: center; height: 100vh;"
-body += "margin: 0; flex-direction: column;} h1 { font-size: 3em; color: black;"
-body += "} </style> </head>"
-body += "<body><h1>"
-body += "See Ya !"
-body += "</h1></body></html>"
-body += "<br><a href=\"/python/welcome.py\"><button> Go back to main page</button></a>"
 
-if "SERVER_NAME" in os.environ:
-    serverName = os.environ.get('SERVER_NAME')
-    print(f"Server: {serverName}")
-date = datetime.datetime.now().astimezone().strftime("%a, %d %b %G %T %Z")
-print(f"Date: {date}")
-print(f"Set-Cookie: user=blbkvvv;Expires=Thu, 01-Jan-1970 00:00:10 GMT;")
-print(f"Set-Cookie: bgcolor=spspspsp;Expires=Thu, 01-Jan-1970 00:00:10 GMT;")
-print(f"Content-Length: {len(body)}\r\n")
-print(f"{body}")
+if __name__ == "__main__":
+    logOut()
