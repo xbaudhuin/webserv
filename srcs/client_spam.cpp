a1f4af330e8ba5d2b4f02aea3246f6216db9e069
Leo Doyen--Clement <ldoyen--@bess-f4r5s5.clusters.42paris.fr>
2024-08-09 16:05:32 +0200

    save test

--- srcs/client_spam.cpp
+++ srcs/client_spam.cpp
@@ -7,21 +7,23 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT 4252
+#define PORT 4250
 
 int main(int argc, char **argv) {
   int sock = 0;
   int long valread;
 
 
-	if (argc != 4) {
-		std::cout << "1st arg = port, 2nd arg = nb client, 3rd arg = request" << std::endl;
-		return 1;
-	}
+	// if (argc != 4) {
+	// 	std::cout << "1st arg = port, 2nd arg = nb client, 3rd arg = request" << std::endl;
+	// 	return 1;
+	// }
   struct sockaddr_in serv_adr;
 	// std::string request = argv[3];
-	std::string request = "GET / HTTP/1.1\r\n";
-	request += "host: Taylor\r\n";
+	std::string request = "DELETE /dumas/";
+	//request += "��@�*v����%��́�Y��L�'��(M�NSl�,��0�'엻;}myU��1��K�A �^����Fr���Z-��C�g�Ze�1xwi��՘'�0���o��Z���Ğ[aBR7���~�e��I�����ڠ@���� %�X�q�{vR��Te��G�n\\\\\\////@�Y�5�rSR";
+	request += " HTTP/1.1\r\n";
+	request += "host: salut\r\n";
 	request += "\r\n";
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     std::cout << "Error: fail to create socket" << std::endl;
@@ -29,19 +31,19 @@ int main(int argc, char **argv) {
   }
   memset(&serv_adr, 0, sizeof(serv_adr));
   serv_adr.sin_family = AF_INET;
-  serv_adr.sin_port = htons(atoi(argv[1]));
+  serv_adr.sin_port = htons(PORT);
 
   if (inet_pton(AF_INET, "127.0.0.1", &serv_adr.sin_addr) <= 0) {
     std::cout << "Error: fail to inet_pton" << std::endl;
     close(sock);
     return (1);
   }
-  for (size_t i = 0; i < atoi(argv[2]) ; i++)
+  for (size_t i = 0; i < 500 ; i++)
   {
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	connect(sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr));
 	send(sock, request.c_str(), request.size(), 0);
-	usleep(50);
+	usleep(100);
   }
   sleep(150);
 
